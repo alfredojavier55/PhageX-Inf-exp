8e590dca0be47fe4c55ebd4b990152adc6368ec7
Thomas Rosendal <thomas.rosendal@sva.se>
2023-08-24 16:38:29 +0200

    Simplify model alittle for this stage
    * removed mortality and aging
    * Added births of piglets on a fixed day
    * Added infection of sows on a fixed day
    * Search for appropriate parameters

--- Inf-exp.R
+++ Inf-exp.R
@@ -1,6 +1,7 @@
-# Modelling Phage-X first model ----
-# Infection experiment ----
-# There is the time of the experiment 60 days ----
+library(SimInf)
+## Modelling Phage-X first model ----
+## Infection experiment ----
+## There is the time of the experiment 60 days ----
 tspan <- seq(from = 1,
              to = 60,
              by = 1)
@@ -9,82 +10,60 @@ tspan <- seq(from = 1,
 compartments <- c("Sp", "Ip", "Sa", "Ia")
 
 ## There are the propensities between each compartment ----
-transitions <- c("@ -> mu * Sp -> Sp",              # birth rate of Sp
-                 "Sp -> Sp * mu -> @",              # natural-dead of Sp
-                 "Ip -> Ip * mu -> @",              # natural-dead of Ip
-                 "Sa -> Sa * mu -> @",              # natural-dead of Sa
-                 "Ia -> Ia * mu -> @",              # natural-dead of Sa
-                 "Sp -> beta * Sp * Ip -> Ip",      # piglet get infected Ip
-                 "Sp -> alpha * Sp -> Sa",          # Susceptible piglet becomes susceptible adult
-                 "Ip -> alpha * Ip -> Ia",          # Infected piglet becomes Infected adult
-                 "Sa -> beta * Sa * Ia -> Ia",      # Adult get infected (horizontal) Ia
-                 "Sa -> gamma * Sa -> @",           # Susceptible adult go to slaughter
-                 "Ia -> gamma * Ia -> @")           # Infected adult go to slaughter
+transitions <- c("Sp -> beta * Sp * (Ip + Ia)/(Sp + Ip + Sa + Ia) -> Ip",      # piglet get infected
+                 "Sa -> beta * Sa * (Ip + Ia)/(Sp + Ip + Sa + Ia) -> Ia",      # Adult get infected
+                 "Ia -> gamma * Ia -> Sa",           # recovery in adults
+                 "Ip -> gamma * Ip -> Sp")           # recovery in piglets
 
-# There are the parameters ----
-gdata <- c(beta  = 1 / 2,
-           beta2 = 0.01,   
-           alpha = 1 / 70,      # time the piglets become adults
-           mu    = 1 / 365*3,   # Simulation is not going to last maybe 6 pregnancies 2.2 pregnancies per year
-           gamma = 1 / 150)     # animals go to slaughter
+## Parameters
+gdata <- c(beta  = 0.2,      # rate of infection
+           gamma = 1 / 17)     # animals recover in about 17 days
 
-# Number of nodes pens on the barn ----
-n <- 5
+## Number of nodes pens on the barn ----
+n <- 1
 
 ## This is the starting condition of the model ----
-u0 <- data.frame(Sp = rep(10,n),
+u0 <- data.frame(Sp = rep(0, n),
                  Ip = rep(0, n),
                  Sa = rep(1, n),
                  Ia = rep(0, n))
 
-# E matrix (select matrix, to do external transfer) ----
+## E matrix (select matrix, to do external transfer) ----
 E <- matrix(c(1,0,0,0,
               1,0,1,0,
-              1,1,1,1), nrow = 4, 
-            dimnames = list(c("Sp","Ip","Sa","Ia"), c("births", "susceptibles", "all")))
+              0,0,1,0,
+              1,1,1,1), nrow = 4,
+            dimnames = list(c("Sp","Ip","Sa","Ia"), c("births", "susceptibles", "susceptible sows", "all")))
 
-# N matrix (shift) ----
-N <- matrix(c(1,0,1,0), nrow = 4, 
-            dimnames = list(c("Sp","Ip","Sa","Ia"), c("get-seed infected")))
+## N matrix (shift) ----
+N <- matrix(c(1,0,1,0), nrow = 4,
+            dimnames = list(c("Sp","Ip","Sa","Ia"), c("seed infection")))
 
-# Two forms of saving the events: ----
-# 1 Movement between compartments (alone, each single event at a time)
-add1 <- data.frame(event = "intTrans", 
-                  time = 5,
-                  node = 1,
+## Birth piglets on day 10
+add1 <- data.frame(event = "enter",
+                  time = 10,
+                  node = seq_len(n),
                   dest = 0,
-                  n = 0,
-                  proportion = 0.1, 
-                  select = 2, # from the E
-                  shift = 1 # from N number of columns with means the states of the compartments
+                  n = 12,
+                  proportion = 0,
+                  select = 1, # from the E (Only susceptible piglets)
+                  shift = 0 # don't shift them at all
                   )
 
-# 1 Movement between compartments and nodes(alone, each single event at a time)
-add2 <- data.frame(event = "extTrans",
-                  time = 10,
-                  node = 1,
-                  dest = 2,
-                  n = 0,
-                  proportion = 0.5,
-                  select = 3,
-                  shift = 0
+## Infect the sows on day 7
+add2 <- data.frame(event = "intTrans",
+                  time = 7,
+                  node = seq_len(n),
+                  dest = 0,
+                  n = 1,
+                  proportion = 0,
+                  select = 3, # Susceptible sows
+                  shift = 1 # shift the sows down according to the 1st column of N
 )
 
 events <- rbind(add1,add2)
 
-#2 Movements between compartments two types insted of doing the rbind
-add <- data.frame(event = c("intTrans", "extTrans"), 
-                  time = c(5, 10),
-                  node = c(1,1),
-                  dest = c(0,2),
-                  n = 0,
-                  proportion = c(0.1, 0.5), 
-                  select = c(2, 3), # from the E
-                  shift = c(1,0) # from N number of columns with means the states of the compartments
-)
-
-events <- add
-
+events
 ## Build the model ----
 model <- mparse(transitions = transitions,
                 compartments = compartments,
@@ -98,11 +77,24 @@ model <- mparse(transitions = transitions,
 ## Run the model ----
 result <- run(model)
 
-# Plot results
-plot(result)
-head(trajectory(result), n=70)
-tail(trajectory(result))
-plot(result, range = 0.95)
+## Find appropriate parameters for the model given some observed prevalence
 
-p <- prevalence(result, Ip+Ia ~ . )
-plot(p)
+## Lets say in the experiment we observe that the prevlance in a pen
+## is 50% on 10 days after the piglets are born:
+distance <- function(result, ...) {
+    abs(prevalence(result, Ia + Ip ~ Ia + Ip + Sa + Ia)$prevalence[20] - 0.50)
+}
+
+fit <- abc(model = model,
+           priors = c(beta ~ uniform(0, 1)),
+           ninit = 1000,
+           npart = 200,
+           distance = distance)
+
+## This is the estimate of the appropriate beta given the system and the target prevalance
+plot(fit)
+
+
+## This is a sample from the posterior beta
+
+plot(run(fit))
